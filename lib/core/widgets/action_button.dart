b78ed6bfb71dec2c729307ed675e5ad7ffdc0d65
Omar Elmanzalawy <omarmanz2002@gmail.com>
2026-01-14 23:22:28 +0200

    fix prominent liquit glass bug on android

--- lib/core/widgets/action_button.dart
+++ lib/core/widgets/action_button.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 class ActionButton extends StatelessWidget {
-  const ActionButton({super.key, required this.label, this.isLiquidGlass = false,required this.onPressed, this.backgroundColor, this.foregroundColor,this.imagePath,this.isDense = false, this.liquidGlassStyle,this.padding,this.liquidGlassSize});
+  const ActionButton({super.key, required this.label, this.isLiquidGlass = false,required this.onPressed, this.backgroundColor, this.foregroundColor,this.imagePath,this.isDense = false, this.liquidGlassStyle,this.padding,this.liquidGlassSize,this.liquidGlasMinimumSize,this.useSmoothRectangleBorder = true});
 
   final String label;
   final VoidCallback onPressed;
@@ -15,21 +15,42 @@ class ActionButton extends StatelessWidget {
   final bool isLiquidGlass;
   final EdgeInsetsGeometry? padding;
   final AdaptiveButtonSize? liquidGlassSize;
-
+  final Size? liquidGlasMinimumSize;
   final AdaptiveButtonStyle? liquidGlassStyle;
-  
+  final bool useSmoothRectangleBorder;
 
   @override
   Widget build(BuildContext context) {
-    return  isLiquidGlass ?
+    return  isLiquidGlass ? imagePath != null ? 
+
+      AdaptiveButton.child(
+        onPressed: onPressed,
+        color: backgroundColor ?? AppColors.primary,
+        padding: padding,
+        minSize: liquidGlasMinimumSize,
+        size: liquidGlassSize ?? AdaptiveButtonSize.medium,
+        style:  liquidGlassStyle != null && liquidGlassStyle == AdaptiveButtonStyle.prominentGlass  && !PlatformInfo.isIOS26OrHigher() ? AdaptiveButtonStyle.filled : liquidGlassStyle ?? AdaptiveButtonStyle.filled,
+        useSmoothRectangleBorder: useSmoothRectangleBorder,
+        child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(imagePath!,width: 18,height: 18,fit: BoxFit.cover,color: Colors.white,),
+                  const SizedBox(width: 8,),
+                  Text(label, style: TextStyle(color: foregroundColor),)
+                ],
+               )
+      )
+    :
       AdaptiveButton(
         onPressed: onPressed,
         label: label,
         color: backgroundColor ?? AppColors.primary,
         textColor: foregroundColor ?? Colors.white,
         padding: padding,
+        minSize: liquidGlasMinimumSize,
         size: liquidGlassSize ?? AdaptiveButtonSize.medium,
-        style: liquidGlassStyle ?? AdaptiveButtonStyle.filled,
+        style:  liquidGlassStyle != null && liquidGlassStyle == AdaptiveButtonStyle.prominentGlass  && !PlatformInfo.isIOS26OrHigher() ? AdaptiveButtonStyle.filled : liquidGlassStyle ?? AdaptiveButtonStyle.filled,
+        useSmoothRectangleBorder: useSmoothRectangleBorder,
       )
      : PlatformElevatedButton(
       padding: isDense ? EdgeInsets.zero : null,
